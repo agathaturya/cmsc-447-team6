9e07470f8661920b667a9803e4f40e77ca2926de
Agatha Turyahikayo <agatha3@umbc.edu>
2020-12-07 19:03:13 -0500

    modified map, bc nyc boroughs are represented differently

--- react-proj/src/map/covid_map.js
+++ react-proj/src/map/covid_map.js
@@ -7,7 +7,6 @@ import d3Selection from "d3-selection";
 import d3Scale from "d3-scale";
 import d3Tip from "d3-tip";
 
-
 var d3_collection = require("d3-collection");
 var d3_legend = require("d3-svg-legend");
 var svg;
@@ -15,14 +14,13 @@ var g;
 var casesColorScale;
 var deathsColorScale;
 
-
 var legend;
 
 //mapData has boundaries and county info
 //covidData has covid data for each fips
 //width and height used for size of svg
 function CovidMap({ mapData, covidData, width, height }) {
-  console.log(mapData);
+  //console.log(mapData);
   var covidDataDict = d3_collection.map(); //used to map fips --> [cases, deaths]
 
   var casesDomain = [1, 10, 100, 1000, 10000, 100000, 1000000]; //domain for cases choropleth colors
@@ -31,7 +29,7 @@ function CovidMap({ mapData, covidData, width, height }) {
     .domain(casesDomain)
     .range(d3.schemeRdPu[7]); //colors used in map
 
-  var deathsDomain = [1,20, 40, 60, 80, 100,1000,7000]; //domain for deaths choropleth colors
+  var deathsDomain = [20, 40, 60, 80, 100, 1000, 7000 ,25000]; //domain for deaths choropleth colors
   deathsColorScale = d3
     .scaleThreshold()
     .domain(deathsDomain)
@@ -62,15 +60,36 @@ function CovidMap({ mapData, covidData, width, height }) {
           covidDataDict.get(d.properties.fips)[0].toLocaleString() +
           " cases, " +
           covidDataDict.get(d.properties.fips)[1].toLocaleString();
-      } catch (e) {
-        str =
-          d.properties.name +
-          " " +
-          d.properties.lsad +
-          ", " +
-          d.properties.state +
-          " \n" +
-          " has no data.";
+      } catch (error) {
+        if (
+          d.properties.state == "New York" &&
+          (d.properties.name == "New York" ||
+            d.properties.name == "Bronx" ||
+            d.properties.name == "Queens" ||
+            d.properties.name == "Kings" ||
+            d.properties.name == "Richmond")
+        ) {
+          str =
+            "New York" +
+            " " +
+            "City" +
+            ", " +
+            d.properties.state +
+            ": \n" +
+            covidDataDict.get("New York City")[0].toLocaleString() +
+            " cases, " +
+            covidDataDict.get("New York City")[1].toLocaleString() + " deaths";
+        } else {
+          str =
+            d.properties.name +
+            " " +
+            d.properties.lsad +
+            ", " +
+            d.properties.state +
+            " \n" +
+            " has no data.";
+        }
+
         return str;
       }
       if (d.deaths == 1) str += " death";
@@ -103,7 +122,14 @@ function CovidMap({ mapData, covidData, width, height }) {
       //mapping fips to [cases, deaths]
       var fips = covidData[x]["fips"];
       fips = i["fips"].toString();
-      covidDataDict.set(fips, [i["cases"], i["deaths"]]);
+
+      if (i["county"] == "New York City") {
+        covidDataDict.set("New York City", [i["cases"], i["deaths"]]);
+        
+        
+      } else {
+        covidDataDict.set(fips, [i["cases"], i["deaths"]]);
+      }
     }
 
     svg = d3.select(ref.current);
@@ -134,11 +160,20 @@ function CovidMap({ mapData, covidData, width, height }) {
         try {
           d.cases = covidDataDict.get(fips)[0];
           d.deaths = covidDataDict.get(fips)[1];
-        } catch (e) {
-         
-          d.cases = 0;
-          d.deaths = 0;
-          return "#666464"
+        } catch (error) {
+          if (
+            d.properties.state == "New York" &&
+            (d.properties.name == "New York" ||
+              d.properties.name == "Bronx" ||
+              d.properties.name == "Queens" ||
+              d.properties.name == "Kings" ||
+              d.properties.name == "Richmond")
+          ) {
+            d.cases = covidDataDict.get("New York City")[0];
+            d.deaths = covidDataDict.get("New York City")[1];
+          } else {
+            return "#666464";
+          }
         }
 
         return casesColorScale(d.cases);
@@ -157,7 +192,6 @@ function CovidMap({ mapData, covidData, width, height }) {
       .attr("class", "legendQuant")
       .attr("transform", "translate(900,20)");
 
-   
     legend = d3_legend
       .legendColor()
       .labelFormat(d3.format(",.0f"))
@@ -180,7 +214,6 @@ function CovidMap({ mapData, covidData, width, height }) {
     svg.call(zoom);
 
     function clicked(d) {
-
       //toggle death counts
       if (showingCases) {
         d3.selectAll(".county")
@@ -188,21 +221,39 @@ function CovidMap({ mapData, covidData, width, height }) {
           .style("opacity", 0.9)
           .attr("fill", function (e) {
             try {
-              console.log(e);
+            
               e.cases = covidDataDict.get(e.properties.fips)[0];
               e.deaths = covidDataDict.get(e.properties.fips)[1];
-            } catch (e) {
+            } catch (error) {
 
-              e.cases = 0;
-              e.deaths = 0;
-              return "#666464"
+
+
+              try{
+              if (
+                e.properties.state == "New York" &&
+                (e.properties.name == "New York" ||
+                  e.properties.name == "Bronx" ||
+                  e.properties.name == "Queens" ||
+                  e.properties.name == "Kings" ||
+                  e.properties.name == "Richmond")
+              ) {
+                e.cases = covidDataDict.get("New York City")[0];
+                e.deaths = covidDataDict.get("New York City")[1];
+              } else {
+                return "#666464";
+              }
+            }
+
+            catch(error){
+              console.log("HEY")
+              console.log(e)
+            }
             }
 
             return deathsColorScale(e.deaths);
           })
           .style("stroke", "transparent")
           .style("opacity", 0.9);
-
 
         //updating legend
         legend = d3_legend
@@ -212,7 +263,8 @@ function CovidMap({ mapData, covidData, width, height }) {
           .labels(function ({ i, genLength, generatedLabels, labelDelimiter }) {
             if (i === 0) {
               const values = generatedLabels[i].split(` ${labelDelimiter} `);
-              return `0  deaths`;
+             // return `Less than ${values[1]}`
+              return `Less than ${values[1]} deaths`;
             } else if (i === genLength - 1) {
               const values = generatedLabels[i].split(` ${labelDelimiter} `);
               return `${values[0]}+  deaths`;
@@ -225,7 +277,7 @@ function CovidMap({ mapData, covidData, width, height }) {
 
         svg.select(".legendQuant").call(legend);
         showingCases = false;
-      } 
+      }
 
       //toggling case counts
       else {
@@ -234,20 +286,29 @@ function CovidMap({ mapData, covidData, width, height }) {
           .style("opacity", 0.9)
           .attr("fill", function (e) {
             try {
-              console.log(e);
+             // console.log(e);
               e.cases = covidDataDict.get(e.properties.fips)[0];
               e.deaths = covidDataDict.get(e.properties.fips)[1];
-            } catch (e) {
-              e.cases = 0;
-              e.deaths = 0;
-              return "#666464"
+            } catch (error) {
+              if (
+                e.properties.state == "New York" &&
+                (e.properties.name == "New York" ||
+                  e.properties.name == "Bronx" ||
+                  e.properties.name == "Queens" ||
+                  e.properties.name == "Kings" ||
+                  e.properties.name == "Richmond")
+              ) {
+                e.cases = covidDataDict.get("New York City")[0];
+                e.deaths = covidDataDict.get("New York City")[1];
+              } else {
+                return "#666464";
+              }
             }
 
             return casesColorScale(e.cases);
           })
           .style("stroke", "transparent")
           .style("opacity", 0.9);
-      
 
         //updating legend
         legend = d3_legend

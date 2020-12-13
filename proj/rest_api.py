e7b5acde459adef35bea161999e8bf6627a1a027
Agatha Turyahikayo <agatha3@umbc.edu>
2020-12-12 20:35:04 -0500

    added endpoint for getting covid prison data

--- proj/rest_api.py
+++ proj/rest_api.py
@@ -18,6 +18,18 @@ mongo_us_covid = PyMongo(app, uri="mongodb://localhost:27017/us_counties_covid_d
 mongo_prison_covid =  PyMongo(app, uri="mongodb://localhost:27017/prison_covid_data")
 cors = CORS(app)
 
+#get covid prison data
+@app.route('/get_prison_covid_data/', methods=['GET'])
+def get_prison_covid_data():
+  result = []
+  doc = mongo_prison_covid.db.prison_covid_data
+  for i in doc.find():
+    print(i)
+    result.append(i)
+
+  return json.dumps(result, default=str)
+
+
 #get prison covid data by date
 #date needs to be formatted like this:YYYY-MM-DD   
 @app.route('/get_prison_covid_data_by_date/<date>/', methods=['GET'])
@@ -244,84 +256,6 @@ def get_us_counties_covid_data_by_deaths_A():
     result.append(i)
   return json.dumps(result, default=str)
 
-#^^^---------------------------^^^
-
-'''
-@app.route('/md_covid_data.json', methods=['POST'])
-def add_data():
-  #each feature contains (new?) covid case counts for a single day, from each county
-  data = json.loads(request.get_json())
-  features = (data['features'])
-  count = 0
-  for i in features:
-    print(i['properties'])
-    print(type(i['properties']))
-    #each feature should be a document in db
-    #{'OBJECTID': 204, 'DATE': '2020/10/04 14:00:00+00', 'Allegany': 492, 'Anne_Arundel': 10376, 'Baltimore': 18354, 'Baltimore_City': 15954, 'Calvert': 1005, 'Caroline': 662, 'Carroll': 2003, 'Cecil': 1120, 'Charles': 2851, 'Dorchester': 614, 'Frederick': 4191, 'Garrett': 76, 'Harford': 3141, 'Howard': 5234, 'Kent': 309, 'Montgomery': 22976, 'Prince_Georges': 30004, 'Queen_Annes': 680, 'Somerset': 299, 'St_Marys': 1343, 'Talbot': 578, 'Washington': 1861, 'Wicomico': 2085, 'Worcester': 1082, 'Unknown': 0}
-    i = i['properties']
-    doc = mongo_covid.db.md_covid_data
-    obj_id = i['OBJECTID']
-    date = i['DATE']
-    allegany = i['Allegany']
-    anne_arundel = i['Anne_Arundel']
-    baltimore = i['Baltimore']#double check
-    baltimore_city = i['Baltimore_City']
-    calvert = i['Calvert']
-    caroline = i['Caroline']
-    carroll = i['Carroll']
-    cecil = i['Cecil']
-    charles = i['Charles']
-    dorchester = i['Dorchester']
-    frederick = i['Frederick']
-    garrett = i['Garrett']
-    harford = i['Harford']
-    howard = i['Howard']
-    kent = i['Kent']
-    montgomery = i['Montgomery']
-    prince_georges = i['Prince_Georges']
-    queen_annes = i['Queen_Annes']
-    st_marys = i['St_Marys']
-    somerset = i['Somerset']
-    talbot = i['Talbot']
-    washington = i['Washington']
-    wicomico = i['Wicomico']
-    worcester = i['Worcester']
-    unknown = i['Unknown']
-
-    
-    doc.insert({'obj_id':obj_id, 'date':date,
-                'counties':{
-    'allegany':allegany,\
-    'anne arundel':anne_arundel,\
-    'baltimore':baltimore, \
-    'baltimore city':baltimore_city, \
-    'calvert':calvert, \
-    'caroline':caroline,\
-    'carroll':carroll,\
-    'cecil':cecil,\
-    'charles':charles,\
-    'dorchester':dorchester,\
-    'frederick':frederick, \
-    'garrett':garrett, \
-    'harford':harford,\
-    'howard':howard,\
-    'kent':kent, \
-    'montgomery':montgomery,\
-    'prince george\'s':prince_georges, \
-    'queen anne\'s':queen_annes,\
-    'st mary\'s':st_marys,\
-    'somerset':somerset,\
-    'talbot':talbot,\
-    'washington':washington,\
-    'wicomico':wicomico, \
-    'worcester':worcester, \
-                  'unknown': unknown}})
-
-    count += 1
-    
-  return jsonify({'num_entries': count});
-'''
-
 #get prison data
 @app.route('/get_prison_data/', methods=['GET'])
 def get_prison_data():
@@ -333,6 +267,7 @@ def get_prison_data():
 
   return json.dumps(result, default=str)
 
+#post prison data
 @app.route('/prison_data.json', methods=['POST'])
 def post_prison_data():
     
